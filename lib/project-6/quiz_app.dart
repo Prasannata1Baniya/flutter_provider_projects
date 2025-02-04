06f339269cd74f44c83c03aced0479dec1913c35
Prasannata Baniya <prasannata250baniya@gmail.com>
2025-02-04 22:22:18 +0545

    Initial Commit

--- lib/project-6/quiz_app.dart
+++ lib/project-6/quiz_app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_provider/project-6/questions_list.dart';
 import 'package:flutter_provider/project-6/quiz_provider.dart';
 import 'package:provider/provider.dart';
 
+
 class QuizApp extends StatefulWidget {
   const QuizApp({super.key});
 
@@ -11,97 +12,85 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  List<QuestionsAnswers> questionsList = questions;
+  List<bool> isClickedList = List.generate(questions.length, (index) => false);
+  List<Color> widthColors = List.generate(questions.length, (index) => Colors.black);
+  PageController pageController = PageController();
+  int page = 0;
 
-
-  List<QuestionsAnswers> questionsList=questions;
-  bool isClicked= false;
-  Color widthColor=Colors.black;
-  PageController pageController=PageController();
-  int page=0;
-
-  void click(int index){
-    setState(() {
-      isClicked=!isClicked;
-    });
-    if(!isClicked){
-      if(questionsList[index].answer.values.contains(true)) {
-        setState(() {
-          isClicked=!isClicked;
-          widthColor=Colors.green;
-        });
-      }else{
-        setState(() {
-          widthColor=Colors.red;
-        });
-      }
+  void click(int index) {
+    if (!isClickedList[index]) {
+      setState(() {
+        isClickedList[index] = true;
+      });
     }
   }
 
   @override
-  void dispose(){
+  void dispose() {
     pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Quiz App",style: TextStyle(color:Colors.white),),
+        title: const Text("Quiz App", style: TextStyle(color: Colors.white)),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body:Consumer<QuizProvider>(
-            builder:(context,value,widget){
-              final quizProvider= Provider.of<QuizProvider>(context);
-              return PageView.builder(
-                controller: pageController,
-            onPageChanged: (val){
-                  setState(() {
-                    page=val;
-                  });
+      body: Consumer<QuizProvider>(
+        builder: (context, quizProvider, widget) {
+          return PageView.builder(
+            controller: pageController,
+            onPageChanged: (val) {
+              setState(() {
+                page = val;
+              });
             },
             itemCount: questionsList.length,
             itemBuilder: (BuildContext context, int index) {
               return ListView(
                 children: [
-
-                  //Question
-                  Text(questionsList[index].question),
-
-                  //Answer
-                  for(int i=0;i<questionsList[index].answer.length;i++)
+                  // Question
+                  Text(questionsList[index].question, style: const TextStyle(color: Colors.black)),
+                  // Answer
+                  for (int i = 0; i < questionsList[index].answer.length; i++)
                     GestureDetector(
-                      onTap: ()=>click(index),
+                      onTap: () {
+                        click(index);
+                        // Check if the answer is correct and track it
+                        String selectedAnswer = questionsList[index].answer.keys.elementAt(i);
+                        quizProvider.trackRightAnswer(index, selectedAnswer);
+                        if (questionsList[index].answer[selectedAnswer] == true) {
+                          quizProvider.userScore();
+                        }
+                      },
                       child: Container(
-                          decoration: BoxDecoration(
-                            border:Border.all(
-                              color: widthColor,
-                            ),
-                          ),
-                          child: Text(questionsList[index].answer.keys.elementAt(i),
-                            style: const  TextStyle(color:Colors.white,
-                            ),
-                          )
+                        decoration: BoxDecoration(
+                          border: Border.all(color: isClickedList[index] ? (questionsList[index].answer.values.elementAt(i) ? Colors.green : Colors.red) : Colors.black),
+                        ),
+                        child: Text(
+                          questionsList[index].answer.keys.elementAt(i),
+                          style: const TextStyle(color: Colors.black),
+                        ),
                       ),
                     ),
-
-                  const SizedBox(height: 10,),
+                  const SizedBox(height: 10),
                   GestureDetector(
-                    onTap:(){
-                      quizProvider.trackRightAnswer();
-                      quizProvider.userScore();
+                    onTap: () {
+                      // You can add any additional logic here if needed
                     },
                     child: Container(
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         color: Colors.black,
                       ),
-                      child: const Text("Submit",style: TextStyle(color:Colors.white),),
+                      child: const Text("Submit", style: TextStyle(color: Colors.white)),
                     ),
                   ),
-                  Text("Total Score:${context.watch<QuizProvider>().scores}"),
+                  Text("Total Score: ${quizProvider.score}"),
                 ],
               );
             },
@@ -109,12 +98,12 @@ class _QuizAppState extends State<QuizApp> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){
-        pageController.nextPage(duration:const  Duration(seconds: 1),
-            curve: Curves.easeIn);
-      },
-      child: const Icon(Icons.next_plan_outlined),
+        onPressed: () {
+          pageController.nextPage(duration: const Duration(seconds: 1), curve: Curves.easeIn);
+        },
+        child: const Icon(Icons.next_plan_outlined),
       ),
     );
   }
 }
+
--- lib/project-6/quiz_provider.dart
+++ lib/project-6/quiz_provider.dart
@@ -1,28 +1,33 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/project-6/questions_list.dart';
 
-class QuizProvider extends ChangeNotifier{
+class QuizProvider extends ChangeNotifier {
+  List<QuestionsAnswers> questionList = questions;
+  final List<QuestionsAnswers> _rightAnswers = [];
+  int _score = 0;
 
-  List<QuestionsAnswers> questionList= questions;
-  List<dynamic> _rightAnswer=[];
-  int scores=0;
+  List<QuestionsAnswers> get rightAnswers => _rightAnswers;
+  int get score => _score;
 
-  List<dynamic> get rightAnswer=>_rightAnswer;
-
-  void trackRightAnswer(){
-      List<dynamic> rightAnswer = questionList.where((element) {
-        var el = element.answer.containsValue(true);
-        return el;
-      }).toList();
-      _rightAnswer = rightAnswer;
+  // Method to track the right answers based on user selections
+  void trackRightAnswer(int questionIndex, String selectedAnswer) {
+    if (questionList[questionIndex].answer[selectedAnswer] == true) {
+      _rightAnswers.add(questionList[questionIndex]);
       notifyListeners();
+    }
   }
 
-  void userScore(){
-    scores++;
-    //scores=_rightAnswer.length;
+  // Method to increment the score
+  void userScore() {
+    _score++;
+    notifyListeners();
+  }
+
+  // Method to reset the quiz state
+  void resetQuiz() {
+    _rightAnswers.clear();
+    _score = 0;
     notifyListeners();
   }
 }
+
